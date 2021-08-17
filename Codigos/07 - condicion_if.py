a494deb3fc3edf1aa23874c1edeeacb2316f5ae5
ElianEstrada <chictibiris@gmail.com>
2021-08-16 21:08:18 -0600

    Clase - 07

---
+++ Codigos/07 - condicion_if.py
@@ -0,0 +1,24 @@
+# Sintaxis del if
+
+# 'if' '(' condicion ')' ':'
+# '    ' bloque_codigo
+
+#Identificar si un número x es positivo
+
+x = 10
+
+if ( x > 0 ):
+    print(f'{x} es positivo')
+
+
+#Identificar si un número x es 
+#positivo o negativo
+
+if (x > 0): 
+    print(f"{x} es positivo")
+
+if (x < 0):
+    print(f"{x} es negativo")
+
+
+print("Adios")
---
+++ Codigos/07 - condicion_if_else.py
@@ -0,0 +1,17 @@
+# Sintaxis del if - else
+
+# 'if' '(' condicion ')' ':'
+# '    ' bloque_codigo
+# 'else' ':'
+# '    ' bloque_codigo
+
+
+#Identificar si un número x es 
+#positivo o negativo
+
+x = -10
+
+if (x > 0): 
+    print(f"{x} es positivo")
+else : 
+    print(f"{x} es negativo")
